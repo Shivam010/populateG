87922176574461113ce6d6043be4da2dbd69d6b5
Shivam Rathore <shivam.rathore010@gmail.com>
2019-05-26 21:16:02 +0530

    google domain verification

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
@@ -16,6 +17,11 @@ func main() {
 	router.GET("/login", Login)
 	router.GET("/home", CallbackHome)
 	router.POST("/process", Process)
+	router.GET("/googleccd40724f8c32619.html", googleVerification)
 
 	log.Fatal(http.ListenAndServe(":"+PORT, router))
+}
+
+func googleVerification(w http.ResponseWriter, r *http.Request, _ httprouter.Params)  {
+	fmt.Fprintf(w,"google-site-verification: googleccd40724f8c32619.html")
 }
